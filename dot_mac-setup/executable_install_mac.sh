e995fdc7ab79ba5a6c5a78becf7d80dcf1e22c70
Martin Møller Jensen <me@mmj.ai>
2022-07-18 14:00:47 +0200

    Add dot_mac-setup/executable_install_mac.sh

---
+++ dot_mac-setup/executable_install_mac.sh
@@ -0,0 +1,19 @@
+# Xcode
+xcode-select --install
+
+# Brew
+if hash brew 2>/dev/null; then
+        which brew
+    else
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Brew Bundle
+brew bundle
+
+# Java SDK Manager
+curl -s "https://get.sdkman.io" | bash
+
+# Unlock Apps
+sudo xattr -r -d com.apple.quarantine "/Applications/*"
+sudo xattr -cr /Applications/*
