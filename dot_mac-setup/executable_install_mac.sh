ca3600aecd904a574334be897082ebe3db5e28a0
Martin Møller Jensen <me@mmj.ai>
2022-07-21 12:40:04 +0200

    Update dot_mac-setup/Brewfile Update dot_mac-setup/executable_install_mac.sh Update dot_zshrc

--- dot_mac-setup/executable_install_mac.sh
+++ dot_mac-setup/executable_install_mac.sh
@@ -17,3 +17,6 @@ curl -s "https://get.sdkman.io" | bash
 # Unlock Apps
 sudo xattr -r -d com.apple.quarantine "/Applications/*"
 sudo xattr -cr /Applications/*
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
